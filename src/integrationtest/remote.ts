33f46c088664ba87a4f27b46b5938ce1b785f43b
Mattia Belletti <mattia.belletti@gmail.com>
2017-07-30 16:25:06 +0200

    Added remote test.

--- src/integrationtest/remote.ts
+++ src/integrationtest/remote.ts
@@ -1,9 +1,14 @@
 import * as assert from "assert";
-import { getRecipes } from "../remoteservices/gw2shinies";
+import { getRecipes as getGW2EfficiencyRecipes } from "../remoteservices/gw2efficiency";
+import { getRecipes as getGW2ShiniesRecipes } from "../remoteservices/gw2shinies";
 
 describe("Remote system", () => {
     it("Can get GW2Shinies recipes", () => {
         // the promise could fail or succeed; the important thing is that it does raise exceptions
-        getRecipes();
+        getGW2ShiniesRecipes();
+    });
+    it("Can get GW2Efficiency recipes", () => {
+        // the promise could fail or succeed; the important thing is that it does raise exceptions
+        getGW2EfficiencyRecipes();
     });
 });
