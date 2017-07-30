2cabafd01ddcc452128a9f0be0bb427557b18cc2
Mattia Belletti <mattia.belletti@gmail.com>
2017-07-30 15:23:07 +0200

    Moving spec data to json

--- src/remoteservices/gw2shinies.spec.ts
+++ src/remoteservices/gw2shinies.spec.ts
@@ -2,6 +2,7 @@ import * as assert from "assert";
 import * as Rx from "rxjs/Rx";
 import { setAlternativeGet } from "../http";
 import * as gw2shinies from "./gw2shinies";
+import * as data from "./gw2shinies.spec.data.json";
 import checkObservable from "./helperspec";
 
 describe("remoteservices/gw2shinies", () => {
@@ -9,7 +10,7 @@ describe("remoteservices/gw2shinies", () => {
         setAlternativeGet((url) =>
             url === "https://www.gw2shinies.com/api/json/forge/" ||
                 url === "https://www.gw2shinies.com/api/json/forge" ?
-                Promise.resolve(JSON.stringify(data)) : Promise.reject(`Unknown url ${url}`));
+                Promise.resolve(JSON.stringify((data as any).gw2shiniesdata)) : Promise.reject(`Unknown url ${url}`));
     });
     afterEach(() => {
         setAlternativeGet(null);
@@ -37,19 +38,3 @@ describe("remoteservices/gw2shinies", () => {
         ]);
     });
 });
-
-const data = [
-    {
-        average_yield: "1",
-        recipe_item_1: "21156",
-        recipe_item_1_quantity: "2",
-        recipe_item_2: "19700",
-        recipe_item_2_quantity: "5",
-        recipe_item_3: "19722",
-        recipe_item_3_quantity: "5",
-        recipe_item_4: "20798",
-        recipe_item_4_quantity: "1",
-        target_recipe: "21260",
-        type: "blueprint",
-    },
-];
