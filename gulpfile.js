4de4bdf8aaf1b2310ee92d0d80e17b2481d2acda
Mattia <mattia.belletti@gmail.com>
2017-08-02 16:43:59 +0200

    Better watch system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp')
+    , path = require('path')
+    , _ = require('lodash')
     , ts = require('gulp-typescript')
     , sourcemaps = require('gulp-sourcemaps')
     , project = ts.createProject('tsconfig.json')
     , tslint = require("gulp-tslint")
+    , newy = require("gulp-newy")
     ;
 
 gulp.task('_compile', () =>
@@ -25,9 +28,25 @@ gulp.task('_compile', () =>
 
 gulp.task('_copy_unchanged', () =>
     gulp.src(['src/**/*.json', 'src/**/*.html'])
+        .pipe(newy((pdir, srcfile, abssrcfile) => {
+            const relfile = path.join.apply(
+                null,
+                _.drop(
+                    _.dropWhile(
+                        abssrcfile.split(path.sep),
+                        value => value != 'src'),
+                    1)
+            );
+            const res = path.join(pdir, 'dist', relfile);
+            return res;
+        }))
         .pipe(gulp.dest('dist/'))
 );
 
 gulp.task('build', ['_compile', '_copy_unchanged']);
 
-gulp.task('watch', () => gulp.watch(['src/**/*.ts', 'src/**/*.json', 'src/**/*.html'], ['build']));
+gulp.task('_watchts', () => gulp.watch(['src/**/*.ts'], ['_compile']));
+
+gulp.task('_watchunchanged', () => gulp.watch(['src/**/*.json', 'src/**/*.html'], ['_copy_unchanged']));
+
+gulp.task('watch', ['_watchts', '_watchunchanged']);
