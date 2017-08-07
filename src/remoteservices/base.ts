7cf8a8f67caa600241766d6cf84e9444e43da2d3
Mattia <mattia.belletti@gmail.com>
2017-08-07 14:32:55 +0200

    Styling.

--- src/remoteservices/base.ts
+++ src/remoteservices/base.ts
@@ -1,7 +1,7 @@
 import * as Rx from "rxjs/Rx";
 
-export function feedObservable<T>(feeder: (observer: Rx.Observer<T[]>) => Promise<void>): Rx.Observable<T[]> {
-    return Rx.Observable.create((observer: Rx.Observer<T[]>) => {
-        feeder(observer).then(() => observer.complete(), (err) => observer.error(err));
-    });
-}
+export const feedObservable = <T>(feeder: (observer: Rx.Observer<T[]>) => Promise<void>): Rx.Observable<T[]> =>
+    Rx.Observable.create((observer: Rx.Observer<T[]>) =>
+        feeder(observer).then(
+            () => observer.complete(),
+            (err) => observer.error(err)));
